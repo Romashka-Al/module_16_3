334e04a3be59fa818f12ddf1923ea4926e6b273f
Romashka-Al <alinashkunova@yandex.ru>
2024-12-23 19:47:26 +0400

    Add files via upload

---
+++ module_16_3.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+users = {'1': 'Имя: Example, возраст: 18'}
+
+
+@app.get('/users')
+async def get_all_users() -> dict:
+    return users
+
+
+@app.post('/user/{username}/{age}')
+async def create_users(username: str, age: int) -> str:
+    user_id = str(int(max(users, key=int)) + 1)
+    users[user_id] = f'Имя: {username}, возраст: {age}'
+    return f'User {user_id} is registered'
+
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_users(user_id: str, username: str, age: int) -> str:
+    users[user_id] = f'Имя: {username}, возраст: {age}'
+    return f'The user {user_id} is updated'
+
+
+@app.delete('/user/{user_id}')
+async def delete_user(user_id: str) -> str:
+    users.pop(user_id)
+    return f'User {user_id} delete'
